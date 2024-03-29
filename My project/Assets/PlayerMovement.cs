9fdb0cf82b47371933635e1908f28ed3f5580687
shuffleV <ronayistvanvajk@gmail.com>
2024-03-29 19:17:39 +0100

    PlayerMovement létrehozása és elkezdése

--- My project/Assets/PlayerMovement.cs
+++ My project/Assets/PlayerMovement.cs
@@ -13,6 +13,9 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown("w"))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector3(0, 14, 0);
+        }
     }
 }
