d84cb6ae0732205915e096ae59d3dfe367def6f2
shuffleV <ronayistvanvajk@gmail.com>
2024-04-02 22:27:32 +0200

    Irányítás

--- My project/Assets/PlayerMovement.cs
+++ My project/Assets/PlayerMovement.cs
@@ -4,18 +4,24 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private Rigidbody2D physics;
+
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        physics = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        if (Input.GetKeyDown("w"))
+        float move = Input.GetAxisRaw("Horizontal");
+        physics.velocity = new Vector2(move * 7f, physics.velocity.y);
+
+        if (Input.GetButtonDown("Jump"))
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector3(0, 14, 0);
+            physics.velocity = new Vector2(physics.velocity.x, 14f);
         }
     }
 }
