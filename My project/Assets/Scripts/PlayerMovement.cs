841524602edd078cc4316def83e11db2b4292f5d
shuffleV <ronayistvanvajk@gmail.com>
2024-04-13 01:12:13 +0200

    Tüskék és halál hozzáadása

---
+++ My project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D physics;
+
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        physics = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        float move = Input.GetAxisRaw("Horizontal");
+        physics.velocity = new Vector2(move * 7f, physics.velocity.y);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            physics.velocity = new Vector2(physics.velocity.x, 16f);
+        }
+    }
+}
